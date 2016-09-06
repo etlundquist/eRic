9132ef97abbbbab79aac1a9f646b417aa66fa561
eric lundquist <e.t.lundquist@gmail.com>
2016-09-06 11:15:35 -0500

    updated varCluster.R

--- R/varCluster.R
+++ R/varCluster.R
@@ -1,4 +1,82 @@
+#' Predictor clustering and cluster summary extraction
+#' 
+#' Perform hierarchical clustering on a set of numeric predictor variables on the basis of 
+#' correlation distance. Create a summary variable for each cluster to use in dimension reduction.
+#' 
+#' @param x (data frame) data frame containing numeric predictors to be clustered
+#' @param y (numeric) response variable (to look at x-y correlations)
+#' @param corr.min (numeric) minimum correlation required to agglomerate variables into clusters
+#' @param clus.summary (character) method used to extract a summary variable for each cluster (see details)
+#' @param corr.method (character) which correlation coefficient to use: c('pearson', 'spearman')
+#' @param corr.use (character) how to deal with missing values: c('complete.obs', 'pairwise.complete.obs')
+#' @param clus.method (character) how to calculate linkages for agglomerative clustering: c('complete', 'single', 'centroid')
+#' 
+#' @details 
+#' Variables will be agglomerated into clusters on the basis of correlation distance (1 - abs(cor)) using
+#' a hierarchical clustering algorithm (hclust). The resulting dendogram will be cut based on the value
+#' of \code{corr.min} supplied by the user, such that all clusters contain variables with linkage correlations
+#' of at least \code{corr.min}. A single summary variable will be created for each cluster based on the value
+#' of \code{clus.summary} supplied by the user:
+#' \item{max.pw} {original variable with highest average pairwise correlation amongst cluster variables}
+#' \item{max.y} {original variable with highest correlation with the response (\code{y})}
+#' \item{avg.x} {average of standardized variables within the cluster (cluster centroid)}
+#' \item{pc.1} {first principal component of cluster variables}
+#' 
+#' @return a list containing the following elements:
+#' \item{nvar} {number of variables in \code{x}}
+#' \item{nclust} {number of created clusters}
+#' \item{clusters} {a list for each cluster containing the number of variables in the cluster, 
+#' the names of the variables in the cluster, all pairwise correlations, and predictor-response correlations)}
+#' \item{summaries} {a data frame containing the summary variables (one for each cluster)}
+#' \item{corrplot} {a ggplot2 object visualizing the correlations amongst all predictor variables}
+#' \item{params} {a list of all parameter values passed in the original call}
+#'  
+#' @examples 
+#' library(dplyr)
+#' library(caret)
+#' data("ChemicalManufacturingProcess")
+#' x <- select(ChemicalManufacturingProcess, -Yield)
+#' y <- ChemicalManufacturingProcess$Yield
+#' res <- varCluster(x, y, corr.min = 0.75, clus.summary = 'pc.1', corr.method = 'spearman', corr.use = 'complete.obs')
+#' res$nvar
+#' res$nclust
+#' res$clusters
+#' str(res$summaries)
+#' res$corrrplot
+#' res$params
+#' @export
+
 varCluster <- function(x, y, corr.min = sqrt(2)/2, clus.summary = 'max.pw', corr.method = 'pearson', corr.use = 'complete.obs', clus.method = 'complete') {
+    
+    # load required packages
+    
+    if (!require(GGally) || !require(magrittr)) {
+        stop('packages [GGally, magrittr] are required for this function')
+    }
+    
+    # check input parameters
+    
+    if (!is.data.frame(x) || !all(sapply(x, typeof) %in% c('integer', 'double'))) {
+        stop('parameter [x] must be a data frame of all numeric variables')
+    }
+    if (!is.numeric(y) || length(y) != nrow(x)) {
+        stop('parameter [y] must be a numeric vector with length equal to nrow(x)')
+    }
+    if (!is.numeric(corr.min) || length(corr.min) != 1 || corr.min < 0 || corr.min > 1) {
+        stop('parameter [corr.min] must be a number between [0,1]')
+    }
+    if (!(clus.summary %in% c('max.pw', 'max.y', 'avg.x', 'pc.1'))) {
+        stop('parameter [clus.summary] must be one of [max.pw, max.y, avg.x, pc.1]')
+    }
+    if (!(corr.method %in% c('pearson', 'spearman'))) {
+        stop('parameter [corr.method] must be one of [pearson, spearman]')
+    }
+    if (!(corr.use %in% c('complete.obs', 'pairwise.complete.obs'))) {
+        stop('parameter [corr.use] must be one of [complete.obs, pairwise.complete.obs]')
+    }
+    if (!(clus.method %in% c('single', 'complete', 'centroid'))) {
+        stop('parameter [clus.method] must be one of [single, complete, centroid]')
+    }
     
     # calculate initial distance matrix and variable clusters
     
@@ -49,7 +127,7 @@ varCluster <- function(x, y, corr.min = sqrt(2)/2, clus.summary = 'max.pw', corr
             
             # calculate average pairwise correlations and correlations with the response
             
-            pw.corr <- cor(cvars, use = corr.use, method = corr.method)
+            pw.corr <- abs(cor(cvars, use = corr.use, method = corr.method))
             diag(pw.corr) <- NA # remove self-correlations
             pw.corr <- apply(pw.corr, 2, mean, na.rm = TRUE)
             res[['clusters']][[cname]][['pw.corr']] <- pw.corr
@@ -60,7 +138,7 @@ varCluster <- function(x, y, corr.min = sqrt(2)/2, clus.summary = 'max.pw', corr
             # calculate the cluster summary variable based on user input
             
             if (clus.summary == 'max.pw') {
-                res[['summaries']][[cname]] <- cvars[,names(pw.corr[abs(pw.corr) == max(abs(pw.corr))][1])]
+                res[['summaries']][[cname]] <- cvars[,names(pw.corr[pw.corr == max(pw.corr)][1])]
             }
             else if (clus.summary == 'max.y') {
                 res[['summaries']][[cname]] <- cvars[,names(y.corr[abs(y.corr) == max(abs(y.corr))][1])]
@@ -81,59 +159,13 @@ varCluster <- function(x, y, corr.min = sqrt(2)/2, clus.summary = 'max.pw', corr
         } # end if
     }     # end for
     
-    # return summary object
+    # create corrplot object and return results
     
+    toplot <- x[,names(sort(clusters))]
+    names(toplot) <- abbreviate(names(sort(clusters)), minlength = 4)
+    res[['corrplot']]  <- ggcorr(toplot, c(corr.use, corr.method), hjust = 1, size = 2, layout.exp = 2)
     res[['summaries']] <- data.frame(res[['summaries']])
     res
     
 }
-            
-library(caret)
-library(AppliedPredictiveModeling)
-library(dplyr)
-library(Information)
 
-data("ChemicalManufacturingProcess")
-chemicals <- ChemicalManufacturingProcess
-
-x <- select(train, -TREATMENT, -PURCHASE)
-y <- train$PURCHASE
-
-corr.min     <- sqrt(2)/2
-clus.summary <- 'max.pw'
-corr.method  <- 'pearson'
-corr.use     <- 'complete.obs'
-clus.method  <- 'complete'
-
-res <- varCluster(x, y, corr.min = 0.5, clus.summary = 'pc.1', corr.method = 'spearman', corr.use = 'pairwise.complete.obs')
-
-
-            
-            
-            
-    
-    
-    
-        
-        names(clusters[clusters == 30])
-        cvars <- x[,names(clusters[clusters == 30]), drop = FALSE]
-        
-        avgpw <- cor(cvars, use = corr.use, method = corr.method)
-        diag(avgpw) <- NA
-        avgpw <- apply(avgpw, 1, mean, na.rm = TRUE)
-        maxpw <- names(avgpw[abs(avgpw) == max(abs(avgpw))])
-        
-        y.corr <- apply(cvars, 2, function(x) cor(x, y, use = corr.use, method = corr.method))
-        maxyc <- names(y.corr[abs(y.corr) == max(abs(y.corr))])
-        
-        pcs <- prcomp(cvars[complete.cases(cvars),], retx = TRUE, center = TRUE, scale = TRUE)
-        pc1 <- rep(NA, nrow(cvars))
-        pc1[complete.cases(cvars)] <- pcs$x[,1] * -1
-        
-        cvars.std <- data.frame(lapply(cvars, function(x) (x-mean(x, na.rm = TRUE))/sd(x, na.rm = TRUE)))
-        cavg      <- apply(cvars.std, 1, mean, na.rm = TRUE)
-        
-        pcs$x[,1]    
-    
-
-sum(distmat[is.na(distmat)])
